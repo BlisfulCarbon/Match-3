c6dcd38471b896dd4c0cf850fef1c59514f9fefd
Aleh <blissful.carbon@gmail.com>
2020-03-24 12:57:49 +0300

    Add scripts generating board

---
+++ Match 3/Assets/Scripts/Board.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    private BackgroundTile[,] allTiles;
+    void Start()
+    {
+        allTiles = new BackgroundTile[width, height];
+        SetUp();
+    }
+
+    private void SetUp(){
+        int countLoop = 1;
+
+        for(int i = 0; i < width; i++){
+            for(int j = 0; j < height; j++){
+                Debug.Log(countLoop);
+                countLoop++;
+                GameObject backgroundTile = Instantiate(tilePrefab, new Vector2(i, j), Quaternion.identity) as GameObject;
+                backgroundTile.transform.parent = this.transform;
+                backgroundTile.name = "(" + i + ", " + j + ")";
+            }
+        }
+    }
+}
